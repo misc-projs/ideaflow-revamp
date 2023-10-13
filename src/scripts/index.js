34b0321a6551e373a42636c0c12658e03bb96926
eshwagadkar <eshwagadkar007@gmail.com>
2023-10-13 19:58:39 +0530

    pre-final commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,8 @@
 // Import the main styles
 import '../styles/main.scss';
 
+import RevealOnScroll from './modules/RevealOnScroll';
+
+let revealOnScroll = new RevealOnScroll();
+
 if(module.hot) { module.hot.accept() }
